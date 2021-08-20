f0ef8b80f6a616091a4826265af71fd0d40f2cd9
Oscar Quiroz <65783932+oscarqochoa@users.noreply.github.com>
2021-08-20 14:28:32 -0500

    Diseñar formularios para el presupuesto y agregar los gastos al array

---
+++ src/components/Formulario.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import Error from './Error';
+import shortid from 'shortid';
+
+const Formulario = ({ agregarNuevoGasto }) => {
+
+    const [nombre, guardarNombre] = useState('');
+    const [cantidad, guardarCantidad] = useState(0);
+    const [error, guardarError] = useState(false);
+
+    // Cuando el usuario agrega un gasto
+    const agregarGasto = e => {
+        e.preventDefault();
+
+        // Validar
+        if (cantidad < 1 || isNaN(cantidad) || nombre.trim() === '') {
+            guardarError(true);
+            return;
+        }
+
+        guardarError(false);
+
+        // Construir el gasto
+        const gasto = {
+            nombre,
+            cantidad,
+            id: shortid.generate()
+        };
+
+        // Pasar el gasto al componente principal
+        agregarNuevoGasto(gasto);
+
+        // Reset form
+        guardarNombre('');
+        guardarCantidad('');
+    }
+
+    return (
+        <form
+            onSubmit={agregarGasto}
+        >
+            <h2>Agregar tus gastos aquí</h2>
+
+            {error ? <Error mensaje="Ambos campos son obligatorios o Presupuesto Incorrecto" /> : null}
+
+            <div className="campo">
+                <label>Nombre Gasto</label>
+                <input
+                    type="text"
+                    className="u-full-width"
+                    placeholder="Ej. Transporte"
+                    value={nombre}
+                    onChange={e => guardarNombre(e.target.value)}
+                />
+            </div>
+
+            <div className="campo">
+                <label>Cantidad Gasto</label>
+                <input
+                    type="number"
+                    className="u-full-width"
+                    placeholder="Ej. 300"
+                    value={cantidad}
+                    onChange={e => guardarCantidad(parseInt(e.target.value))}
+                />
+            </div>
+
+            <input
+                type="submit"
+                className="button-primary u-full-width"
+                value="Agregar gasto"
+            />
+        </form>
+    );
+}
+
+export default Formulario;
---
+++ src/components/Listado.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Listado = () => (
+    <div className="gastos-realizados">
+        <h2>Listado</h2>
+    </div>
+);
+
+export default Listado;
